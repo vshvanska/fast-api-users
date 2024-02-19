c937ca5e112ea4dd896c933a7253448fa70c73ce
vshvanska <ladyvaleriya13@gmail.com>
2024-02-19 15:03:20 +0200

    alembic

--- db/models.py
+++ db/models.py
@@ -1,22 +1,18 @@
 import datetime
 
-from sqlalchemy import String, DateTime, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy import Integer, String, DateTime, func, Column
+from sqlalchemy.orm import Mapped, mapped_column
+from db.engine import Base
 
 
 class User(Base):
-    __tablename__ = "user_account"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String(30), nullable=False, unique=True)
-    email: Mapped[[str]] = mapped_column(nullable=False, unique=True)
-    password: Mapped[str] = mapped_column(nullable=False)
-    register_date: Mapped[datetime.datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
-    )
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(30), nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    register_date = Column(DateTime(timezone=True), server_default=func.now())
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.username!r})"
